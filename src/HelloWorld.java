26752701a6734542dd43dd5e2a0290c5a0cba213
scouca <71228882+scouca@users.noreply.github.com>
2021-08-24 20:29:55 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+
+        System.out.println("----");
+        System.out.println("Teller til ti");
+        for(int i=0; i<=10; i++){
+            System.out.print(i+" ");
+        }
+    }
+}
